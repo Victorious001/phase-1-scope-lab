4e237d927c8019d0cc259b430378534db6393b51
Victorious <new@mail.com>
2022-08-20 11:05:03 +0300

    commit tree 1a088a9aa01ec2b54f46d5246531545f0dea961e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    return customerName = "BOB"
+}
+
+function setBestCustomer() {
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob"
+}
+const leastFavoriteCustomer = "steve"
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "sam"
+}
